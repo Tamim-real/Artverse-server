49283f090bfba23a3507d275820d44db5bad69e1
Tamim-real <tamimislam2653@gmail.com>
2025-11-13 22:27:39 +0600

    final version unveiled

--- index.js
+++ index.js
@@ -28,154 +28,154 @@ async function run() {
     const artCollection = db.collection('artgallery');
     const myFavorites = db.collection('myfavorites');
 
-    app.get('/all-arts', async(req, res)=>{
+    app.get('/all-arts', async (req, res) => {
 
       const result = await artCollection.find({ visibility: "Public" }).toArray();
       res.send(result)
     })
 
-     app.get('/latest-arts', async(req, res)=>{
+    app.get('/latest-arts', async (req, res) => {
 
-      const result = await artCollection.find({ visibility: "Public" }).sort({created_at : -1}).limit(6).toArray();
+      const result = await artCollection.find({ visibility: "Public" }).sort({ created_at: -1 }).limit(6).toArray();
       res.send(result)
     })
 
 
 
     app.get('/all-arts/:id', async (req, res) => {
-  const id = req.params.id;
-  const result = await artCollection.findOne({ _id: new ObjectId(id) });
-  res.send(result);
-});
+      const id = req.params.id;
+      const result = await artCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
 
-app.put("/all-arts/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
+    app.put("/all-arts/:id", async (req, res) => {
+      try {
+        const { id } = req.params;
 
-    if (!ObjectId.isValid(id)) {
-      return res.status(400).send({ message: "Invalid ID format" });
-    }
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ message: "Invalid ID format" });
+        }
 
-    const data = req.body;
-
-    
-    if (data._id) delete data._id;
-
-    const filter = { _id: new ObjectId(id) };
-    const update = { $set: data };
-
-    const result = await artCollection.updateOne(filter, update);
-
-    if (result.matchedCount === 0) {
-      return res.status(404).send({ message: "Artwork not found" });
-    }
-
-    res.send({ _id: id, ...data });
-  } catch (error) {
-    
-    res.status(500).send({ message: "Internal Server Error" });
-  }
-});
+        const data = req.body;
 
 
+        if (data._id) delete data._id;
 
-app.put("/all-arts/like/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const email = req.body.email;
-   
+        const filter = { _id: new ObjectId(id) };
+        const update = { $set: data };
 
-    const art = await artCollection.findOne({ _id: new ObjectId(id) });
-   
+        const result = await artCollection.updateOne(filter, update);
 
-    art.likes = art.likes || []; 
-    let updatedLikes;
-    if (art.likes.includes(email)) {
-      updatedLikes = art.likes.filter(e => e !== email);
-    } else {
-      updatedLikes = [...art.likes, email]; 
-    }
+        if (result.matchedCount === 0) {
+          return res.status(404).send({ message: "Artwork not found" });
+        }
 
-    await artCollection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: { likes: updatedLikes } }
-    );
+        res.send({ _id: id, ...data });
+      } catch (error) {
 
-    const updatedArt = await artCollection.findOne({ _id: new ObjectId(id) });
-    res.send(updatedArt);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Server error", error: err.message });
-  }
-});
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
 
 
 
+    app.put("/all-arts/like/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const email = req.body.email;
 
 
-  app.get('/artist-arts/:email', async(req, res)=>{
-    const email = req.params.email;
-
-    try{
-      const count = await artCollection.countDocuments({userEmail : email})
-      res.send({ totalArtworks: count }); 
-    }catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Failed to fetch artworks count" });
-  }
-  })
+        const art = await artCollection.findOne({ _id: new ObjectId(id) });
 
 
-app.get("/myart/:email", async (req, res) => {
-  const email = req.params.email; 
-  try {
-    const artworks = await artCollection.find({ userEmail: email }).toArray();
-    res.send(artworks);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: "Failed to fetch artworks" });
-  }
-});
+        art.likes = art.likes || [];
+        let updatedLikes;
+        if (art.likes.includes(email)) {
+          updatedLikes = art.likes.filter(e => e !== email);
+        } else {
+          updatedLikes = [...art.likes, email];
+        }
 
-app.delete('/all-arts/:id', async(req, res)=>{
-    const id = req.params.id;
-    const result = await artCollection.deleteOne({ _id: new ObjectId(id) })
-    res.send(result)
-})
+        await artCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { likes: updatedLikes } }
+        );
+
+        const updatedArt = await artCollection.findOne({ _id: new ObjectId(id) });
+        res.send(updatedArt);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Server error", error: err.message });
+      }
+    });
 
 
-    app.post('/all-arts', async(req, res)=>{
+
+
+
+    app.get('/artist-arts/:email', async (req, res) => {
+      const email = req.params.email;
+
+      try {
+        const count = await artCollection.countDocuments({ userEmail: email })
+        res.send({ totalArtworks: count });
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to fetch artworks count" });
+      }
+    })
+
+
+    app.get("/myart/:email", async (req, res) => {
+      const email = req.params.email;
+      try {
+        const artworks = await artCollection.find({ userEmail: email }).toArray();
+        res.send(artworks);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Failed to fetch artworks" });
+      }
+    });
+
+    app.delete('/all-arts/:id', async (req, res) => {
+      const id = req.params.id;
+      const result = await artCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
+
+    app.post('/all-arts', async (req, res) => {
       const data = req.body
       const result = await artCollection.insertOne(data)
       res.send(result)
     })
 
-    app.post('/favorites', async(req, res)=>{
+    app.post('/favorites', async (req, res) => {
       const data = req.body;
       const result = await myFavorites.insertOne(data)
       res.send(result)
     })
 
-    app.get('/my-favorites', async(req, res)=>{
+    app.get('/my-favorites', async (req, res) => {
       const email = req.query.email
-      const result = await myFavorites.find({ favorite_by : email}).toArray()
+      const result = await myFavorites.find({ favorite_by: email }).toArray()
       res.send(result)
     })
-    app.delete('/my-favorites/:id', async(req, res)=>{
-    const id = req.params.id;
-    console.log("Deleting favorite with ID:", id);
-    const result = await myFavorites.deleteOne({ _id: id })
-    console.log(result);
-    
-    res.send(result)
-})
+    app.delete('/my-favorites/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log("Deleting favorite with ID:", id);
+      const result = await myFavorites.deleteOne({ _id: id })
+      console.log(result);
+
+      res.send(result)
+    })
 
 
     // await client.db("admin").command({ ping: 1 });
     // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
-    
+
+
   }
 }
 run().catch(console.dir);
