1d647ecc792d56d03df04e4a5f68404ea32712b0
Tamim-real <tamimislam2653@gmail.com>
2026-07-15 22:34:10 +0600

    CORS issue fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config(); 
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -247,6 +247,10 @@ app.get('/', (req, res) => {
   res.send('Artverse Server API is running smoothly.')
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
+module.exports = app;
